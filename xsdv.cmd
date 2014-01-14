9b3ebc29393ce69a1e72916ed43aaf6e0c0b29fa
jasir <sirjarda2@gmail.com>
2014-01-14 16:37:06 +0100

    Fixed quotes in xsdv.cmd

--- xsdv.cmd
+++ xsdv.cmd
@@ -1,7 +1,7 @@
 @rem Utility script to call xsdv with the given arguments
 @echo off
 IF DEFINED JAVA_HOME (
-%JAVA_HOME%\bin\java.exe -cp "%~dp0\lib\xsdv.jar" xsdvalidator.validate %*
+"%JAVA_HOME%\bin\java.exe" -cp "%~dp0\lib\xsdv.jar" xsdvalidator.validate %*
 ) ELSE (
 java -cp "%~dp0\lib\xsdv.jar" xsdvalidator.validate %*
 )
